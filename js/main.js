305ed75c62a460d66ab941470c6bd149ad20f8ff
annguyen210 <ducanbn810@gmail.com>
2025-04-13 11:28:56 +0700

    45678

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // kiem tra neu chua co nguoi dung thi chuyen huong ve trang login.html
-document.addEventListener("DOMContentLoaded", function () {
+ document.addEventListener("DOMContentLoaded", function () {
   const username = localStorage.getItem("username");
   const password = localStorage.getItem("password");
 
   const userInfoDiv = document.getElementById("user-info");
 
-  if (username) {
+ /* if (username) {
     userInfoDiv.innerHTML = `  
         <p>Chào mừng: <strong>${username}</strong></p>  
         <p>Mật khẩu của bạn là: <strong>${password}</strong></p>  
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
   } else {
     userInfoDiv.innerHTML = "<p>Vui lòng đăng nhập để truy cập trang này.</p>";
     window.location.href = "login.html"; // Nếu không đăng nhập, chuyển hướng về trang đăng nhập
-  }
+  } */
 
   document
     .getElementById("logout-button")
